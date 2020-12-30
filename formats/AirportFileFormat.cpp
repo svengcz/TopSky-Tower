fe052cacff7aaaf797e697712174cf667d193b8c
Sven Czarnian <devel@svcz.de>
2020-12-30 01:55:09 +0100

    introduce the airports file format

---
+++ formats/AirportFileFormat.cpp
@@ -0,0 +1,57 @@
+/*
+ * Author:
+ *   Sven Czarnian <devel@svcz.de>
+ * Brief:
+ *   Implements the euroscope file format
+ */
+
+#include <filesystem>
+
+#include <helper/Exception.h>
+#include <helper/String.h>
+#include <formats/AirportFileFormat.h>
+
+#include "IniFileFormat.h"
+
+using namespace topskytower;
+using namespace topskytower::formats;
+
+void AirportFileFormat::parseSid(const std::string& line, types::StandardInstrumentDeparture& sid) {
+    if (std::string::npos == line.find("SID:"))
+        return;
+
+    auto split = helper::String::splitString(line, ":");
+    if (3 != split.size())
+        return;
+
+    sid.name = split[1];
+    sid.clearanceLimit = static_cast<std::uint16_t>(std::atoi(split[2].c_str()));
+}
+
+AirportFileFormat::AirportFileFormat(const std::string& filename) :
+        m_configurations() {
+    IniFileFormat file(filename);
+
+    for (const auto& block : std::as_const(file.m_blocks)) {
+        std::string icao(block.first);
+        helper::String::stringReplace(icao, "[", "");
+        helper::String::stringReplace(icao, "]", "");
+
+        this->m_configurations[icao].valid = true;
+        for (const auto& line : std::as_const(block.second)) {
+            types::StandardInstrumentDeparture sid;
+
+            this->parseSid(line, sid);
+            this->m_configurations[icao].sids[sid.name] = sid;
+        }
+    }
+}
+
+const types::AirportConfiguration& AirportFileFormat::configuration(const std::string& icao) const {
+    static types::AirportConfiguration fallback;
+    auto it = this->m_configurations.find(icao);
+    if (this->m_configurations.cend() != it)
+        return it->second;
+    else
+        return fallback;
+}
---
+++ include/formats/AirportFileFormat.h
@@ -0,0 +1,42 @@
+/*
+ * @brief Defines the airport file format
+ * @file configuration/AirportFileFormat.h
+ * @author Sven Czarnian <devel@svcz.de>
+ */
+
+#pragma once
+
+#include <map>
+
+#include <formats/EseFileFormat.h>
+#include <types/AirportConfiguration.h>
+
+namespace topskytower {
+    namespace formats {
+        /**
+         * @brief Defines the parser of airport configurations
+         * @ingroup format
+         */
+        class AirportFileFormat {
+        private:
+            std::map<std::string, types::AirportConfiguration> m_configurations;
+
+            void parseSid(const std::string& line, types::StandardInstrumentDeparture& sid);
+
+        public:
+            /**
+             * @brief Parses an airport configuration
+             * If the file does not exist, the constructor throws an exeption.
+             * @param[in] filename The filename of the configuration
+             */
+            AirportFileFormat(const std::string& filename);
+
+            /**
+             * @brief Returns the airport configuration of a special ICAO
+             * @param[in] icao The airport's ICAO code
+             * @return The airport configuration
+             */
+            const types::AirportConfiguration& configuration(const std::string& icao) const;
+        };
+    }
+}
