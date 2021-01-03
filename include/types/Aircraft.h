89f36c68cbd6a6cc2cbc53763d2f2b0ce040c75c
Sven Czarnian <devel@svcz.de>
2021-01-03 11:35:41 +0100

    introduce a class to describe aircrafts

---
+++ include/types/Aircraft.h
@@ -0,0 +1,134 @@
+/*
+ * @brief Defines an aircraft structure
+ * @file types/Aircraft.h
+ * @author Sven Czarnian <devel@svcz.de>
+ */
+
+#pragma once
+
+#include <string>
+
+#include <types/Quantity.hpp>
+
+namespace topskytower {
+    namespace types {
+        /**
+         * @brief Describes an aircraft with all information
+         * @ingroup types
+         */
+        class Aircraft {
+        public:
+            /**
+             * @brief Defines the different wake turbulence categories
+             */
+            enum class WTC {
+                Unknown = 0, /**< Unknown category */
+                Light   = 1, /**< Light aircraft category (i.e. Cessna C172) */
+                Medium  = 2, /**< Medium aircraft category (i.e. Airbus A320) */
+                Heavy   = 3, /**< Heavy aircraft category (i.e. Boeing B747) */
+                Super   = 4  /**< Super aircraft category (i.e. Airbus A380) */
+            };
+            /**
+             * @brief Defines the engine type
+             */
+            enum class EngineType {
+                Unknown   = 0, /**< Unknown engine type */
+                Turboprop = 1, /**< Turboprobs in use */
+                Jet       = 2  /**< Jets in use */
+            };
+
+        private:
+            std::string   m_code;
+            EngineType    m_engineType;
+            std::uint8_t  m_engineCount;
+            WTC           m_wtc;
+            types::Length m_length;
+            types::Length m_wingspan;
+            types::Length m_height;
+            types::Mass   m_maxTakeoffWeight;
+
+        public:
+            /**
+             * @brief Creates an uninitialized aircraft
+             */
+            Aircraft();
+
+            /**
+             * @brief Sets the ICAO code
+             * @param[in] code The ICAO code
+             */
+            void setIcaoCode(const std::string& code);
+            /**
+             * @brief Returns the ICAO code
+             * @return The ICAO code
+             */
+            const std::string& icaoCode() const;
+            /**
+             * @brief Sets the engine type
+             * @param[in] type The engine type
+             */
+            void setEngineType(EngineType type);
+            /**
+             * @brief Returns the engine type
+             * @return The engine type
+             */
+            EngineType engineType() const;
+            /**
+             * @brief Sets the engine count
+             * @param[in] count The engine count
+             */
+            void setEngineCount(std::uint8_t count);
+            /**
+             * @brief Returns the engine count
+             * @return The engine count
+             */
+            std::uint8_t engineCount() const;
+            /**
+             * @brief Sets the WTC
+             * @param[in] wtc The wake turbulence category
+             */
+            void setWTC(WTC wtc);
+            WTC wtc() const;
+            /**
+             * @brief Sets the length
+             * @param[in] length The aircraft's length
+             */
+            void setLength(const types::Length& length);
+            /**
+             * @brief Returns the aircraft's length
+             * @return The aircraft's length
+             */
+            const types::Length& length() const;
+            /**
+             * @brief Sets the wingpsan
+             * @param[in] wingspan The aircraft's wingspan
+             */
+            void setWingspan(const types::Length& wingspan);
+            /**
+             * @brief Returns the aircraft's wingspan
+             * @return The aircraft's wingspan
+             */
+            const types::Length& wingspan() const;
+            /**
+             * @brief Sets the height
+             * @param[in] height The aircraft's height
+             */
+            void setHeight(const types::Length& height);
+            /**
+             * @brief Returns the aircraft's height
+             * @return The aircraft's height
+             */
+            const types::Length& height() const;
+            /**
+             * @brief Sets the MTOW
+             * @param[in] mtow The maximum take-off weight
+             */
+            void setMTOW(const types::Mass& mtow);
+            /**
+             * @brief Returns the aircraft's MTOW
+             * @return The aircraft's maximum take-off weight
+             */
+            const types::Mass& mtow() const;
+        };
+    }
+}
---
+++ types/Aircraft.cpp
@@ -0,0 +1,85 @@
+/*
+ * Author:
+ *   Sven Czarnian <devel@svcz.de>
+ * Brief:
+ *   Implements the aircraft structure
+ */
+
+#include <types/Aircraft.h>
+
+using namespace topskytower;
+using namespace topskytower::types;
+
+Aircraft::Aircraft() :
+        m_code(),
+        m_engineType(types::Aircraft::EngineType::Unknown),
+        m_engineCount(0),
+        m_wtc(types::Aircraft::WTC::Unknown),
+        m_length(),
+        m_wingspan(),
+        m_height(),
+        m_maxTakeoffWeight() { }
+
+void Aircraft::setIcaoCode(const std::string& code) {
+    this->m_code = code;
+}
+
+const std::string& Aircraft::icaoCode() const {
+    return this->m_code;
+}
+
+void Aircraft::setEngineType(Aircraft::EngineType type) {
+    this->m_engineType = type;
+}
+
+Aircraft::EngineType Aircraft::engineType() const {
+    return this->m_engineType;
+}
+
+void Aircraft::setEngineCount(std::uint8_t count) {
+    this->m_engineCount = count;
+}
+
+std::uint8_t Aircraft::engineCount() const {
+    return this->m_engineCount;
+}
+
+void Aircraft::setWTC(Aircraft::WTC wtc) {
+    this->m_wtc = wtc;
+}
+
+Aircraft::WTC Aircraft::wtc() const {
+    return this->m_wtc;
+}
+
+void Aircraft::setLength(const types::Length& length) {
+    this->m_length = length;
+}
+
+const types::Length& Aircraft::length() const {
+    return this->m_length;
+}
+
+void Aircraft::setWingspan(const types::Length& wingspan) {
+    this->m_wingspan = wingspan;
+}
+
+const types::Length& Aircraft::wingspan() const {
+    return this->m_wingspan;
+}
+
+void Aircraft::setHeight(const types::Length& height) {
+    this->m_height = height;
+}
+
+const types::Length& Aircraft::height() const {
+    return this->m_height;
+}
+
+void Aircraft::setMTOW(const types::Mass& mtow) {
+    this->m_maxTakeoffWeight = mtow;
+}
+
+const types::Mass& Aircraft::mtow() const {
+    return this->m_maxTakeoffWeight;
+}
