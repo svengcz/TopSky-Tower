d142d1f83ffca732165ce823e69704bfa97422d4
Sven Czarnian <devel@svcz.de>
2021-01-04 16:01:00 +0100

    introduce a flight plan control that validates the flight plans

---
+++ include/surveillance/FlightPlanControl.h
@@ -0,0 +1,93 @@
+/*
+ * @brief Defines a flight plan control system
+ * @file surveillance/FlightPlanControl.h
+ * @author Sven Czarnian <devel@svcz.de>
+ */
+
+#pragma once
+
+#include <list>
+#include <map>
+#include <string>
+
+#include <types/Flight.h>
+
+namespace topskytower {
+    namespace surveillance {
+        /**
+         * @brief Describes a flight plan control system
+         * @ingroup surveillance
+         */
+        class FlightPlanControl {
+        public:
+            /**
+             * @brief Define sthe error codes after a flight plan validation
+             */
+            enum class ErrorCode {
+                Unknown        = 0x00, /**< The flight plan is not analyzed */
+                VFR            = 0x01, /**< It is an VFR flight */
+                NoError        = 0x02, /**< No error found */
+                Route          = 0x03, /**< No valid route found */
+                DepartureRoute = 0x04, /**< The SID is unknown */
+                EngineType     = 0x05, /**< The engine type does not match */
+                Navigation     = 0x06, /**< The navigation type (RNAV) does not match */
+                Transponder    = 0x07, /**< The transponder does not match */
+                FlightLevel    = 0x08, /**< The minimum or maximum flight level does not match */
+                EvenOddLevel   = 0x09, /**< The flight level does not match to the even/odd constraint */
+            };
+
+        private:
+            struct FlightPlanStatus {
+                std::list<ErrorCode>    errorCodes;
+                bool                    overwritten;
+                std::string             destination;
+                std::string             departureRoute;
+                types::FlightPlan::Type type;
+            };
+
+            std::map<std::string, FlightPlanStatus> m_flightChecks;
+
+            FlightPlanControl();
+
+        public:
+            /**
+             * Destroys all internal structures
+             */
+            ~FlightPlanControl();
+
+            /**
+             * @brief The flight plan of a flight will be validated
+             * @param[in] flight The converted flight structure
+             * @return True if a complete validation run was performed, else false (i.e. no FP change detected)
+             */
+            bool validate(const types::Flight& flight);
+            /**
+             * @brief Removes a flight out of the list
+             * @param[in] callsign The flight's callsign
+             */
+            void removeFlight(const std::string& callsign);
+            /**
+             * @brief Marks an invalid flight plan as overwritten by the controller
+             * @param[in] callsign The flight's callsign
+             */
+            void overwrite(const std::string& callsign);
+            /**
+             * @brief Returns the error codes of a flight plan
+             * @param[in] callsign The flight's callsign
+             * @return The error codes of the corresponding flight plan
+             */
+            const std::list<ErrorCode>& errorCodes(const std::string& callsign) const;
+            /**
+             * @brief Returns if a check is overwritten or not
+             * @param[in] callsign The flight's callsign
+             * @return True if the check is overwritten, else false
+             */
+            bool overwritten(const std::string& callsign) const;
+            /**
+             * @brief Returns the static instance of the flight plan control
+             * @return The flight plan control
+             */
+            static FlightPlanControl& instance();
+        };
+    }
+}
---
+++ surveillance/FlightPlanControl.cpp
@@ -0,0 +1,167 @@
+/*
+ * Author:
+ *   Sven Czarnian <devel@svcz.de>
+ * Brief:
+ *   Implements the sector control system
+ */
+
+#include <algorithm>
+
+#include <surveillance/FlightPlanControl.h>
+#include <system/ConfigurationRegistry.h>
+
+using namespace topskytower;
+using namespace topskytower::surveillance;
+using namespace topskytower::types;
+
+FlightPlanControl::FlightPlanControl() :
+        m_flightChecks() { }
+
+FlightPlanControl::~FlightPlanControl() {
+    this->m_flightChecks.clear();
+}
+
+bool FlightPlanControl::validate(const types::Flight& flight) {
+    /* validate the input flight plan */
+    if (0 == flight.flightPlan().destination().length())
+        return false;
+    if (0 == flight.flightPlan().departureRoute().length())
+        return false;
+    if (types::FlightPlan::Type::Unknown == flight.flightPlan().type())
+        return false;
+
+    auto it = this->m_flightChecks.find(flight.callsign());
+
+    /* check if a new validation is required */
+    bool validationRequired = this->m_flightChecks.end() == it;
+    if (false == validationRequired) {
+        validationRequired |= flight.flightPlan().type() != it->second.type;
+        validationRequired |= flight.flightPlan().departureRoute() != it->second.departureRoute;
+        validationRequired |= flight.flightPlan().destination() != it->second.destination;
+    }
+    else {
+        this->m_flightChecks[flight.callsign()] = {
+            {},
+            false,
+            "",
+            "",
+            types::FlightPlan::Type::Unknown
+        };
+
+        it = this->m_flightChecks.find(flight.callsign());
+    }
+
+    /* validate the flight plan */
+    if (true == validationRequired) {
+        /* update the information */
+        it->second.destination = flight.flightPlan().destination();
+        it->second.departureRoute = flight.flightPlan().departureRoute();
+        it->second.type =  flight.flightPlan().type();
+        it->second.overwritten = false;
+        it->second.errorCodes.clear();
+
+        if (types::FlightPlan::Type::VFR == flight.flightPlan().type()) {
+            it->second.errorCodes.push_back(ErrorCode::VFR);
+        }
+        else {
+            const auto& config = system::ConfigurationRegistry::instance().airportConfiguration(flight.flightPlan().origin());
+
+            /* no configuration for the SID found */
+            auto sit = config.sids.find(it->second.departureRoute);
+            if (config.sids.cend() == sit) {
+                it->second.errorCodes.push_back(ErrorCode::DepartureRoute);
+                return validationRequired;
+            }
+
+            /* the engine-type needs to be checked */
+            if (types::Aircraft::EngineType::Unknown != sit->second.engineType) {
+                if (sit->second.engineType != flight.flightPlan().aircraft().engineType())
+                    it->second.errorCodes.push_back(ErrorCode::EngineType);
+            }
+
+            /* RNAV is required */
+            if (true == sit->second.requiresRnav && false == flight.flightPlan().rnavCapable())
+                it->second.errorCodes.push_back(ErrorCode::Navigation);
+
+            /* transponder is required */
+            if (true == sit->second.requiresTransponder && false == flight.flightPlan().transponderExists())
+                it->second.errorCodes.push_back(ErrorCode::Transponder);
+
+            /* check the flight level constraints */
+            if (sit->second.minimumCruiseLevel > flight.flightPlan().flightLevel() || sit->second.maximumCruiseLevel < flight.flightPlan().flightLevel())
+                it->second.errorCodes.push_back(ErrorCode::FlightLevel);
+
+            /* prepare for the even-odd checks */
+            bool even = 0 == static_cast<int>(flight.flightPlan().flightLevel().convert(types::feet)) % 2;
+
+            /* check if a destination specific constraint is defined */
+            bool foundDestinationConstraint = false;
+            for (const auto& constraint : std::as_const(config.destinationConstraints)) {
+                if (constraint.destination == flight.flightPlan().destination()) {
+                    if (true == constraint.evenCruiseLevel && false == even || false == constraint.evenCruiseLevel && true == even)
+                        it->second.errorCodes.push_back(ErrorCode::EvenOddLevel);
+
+                    foundDestinationConstraint = true;
+                    break;
+                }
+            }
+
+            if (false == foundDestinationConstraint) {
+                if (0 == flight.flightPlan().route().waypoints().size()) {
+                    it->second.errorCodes.push_back(ErrorCode::Route);
+                }
+                else {
+                    /* find the bearing between start and destination */
+                    const auto& waypoints = flight.flightPlan().route().waypoints();
+                    auto bearing = waypoints[0].position().bearingTo(waypoints.back().position());
+
+                    /* use the half circle rule*/
+                    if (180_deg > bearing && true == even || 180_deg <= bearing && false == even)
+                        it->second.errorCodes.push_back(ErrorCode::EvenOddLevel);
+                }
+            }
+
+            /* no error found */
+            if (0 == it->second.errorCodes.size())
+                it->second.errorCodes.push_back(ErrorCode::NoError);
+        }
+    }
+
+    return validationRequired;
+}
+
+void FlightPlanControl::removeFlight(const std::string& callsign) {
+    auto it = this->m_flightChecks.find(callsign);
+    if (this->m_flightChecks.end() != it)
+        this->m_flightChecks.erase(it);
+}
+
+void FlightPlanControl::overwrite(const std::string& callsign) {
+    auto it = this->m_flightChecks.find(callsign);
+    if (this->m_flightChecks.end() != it)
+        it->second.overwritten = true;
+}
+
+const std::list<FlightPlanControl::ErrorCode>& FlightPlanControl::errorCodes(const std::string& callsign) const {
+    static std::list<FlightPlanControl::ErrorCode> fallback{ FlightPlanControl::ErrorCode::Unknown };
+    auto it = this->m_flightChecks.find(callsign);
+
+    if (this->m_flightChecks.cend() != it)
+        return it->second.errorCodes;
+    else
+        return fallback;
+}
+
+bool FlightPlanControl::overwritten(const std::string& callsign) const {
+    auto it = this->m_flightChecks.find(callsign);
+
+    if (this->m_flightChecks.cend() != it)
+        return it->second.overwritten;
+    else
+        return false;
+}
+
+FlightPlanControl& FlightPlanControl::instance() {
+    static FlightPlanControl instance;
+    return instance;
+}
