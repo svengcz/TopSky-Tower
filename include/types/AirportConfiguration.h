8d6aa3188d723c2c5813c1784a7ae6bb056e1ec0
Sven Czarnian <devel@svcz.de>
2021-01-06 12:00:16 +0100

    introduce a stand definition for the configuration block

--- include/types/AirportConfiguration.h
+++ include/types/AirportConfiguration.h
@@ -8,6 +8,7 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
 #include <types/Aircraft.h>
 #include <types/Quantity.hpp>
@@ -40,6 +41,25 @@ namespace topskytower {
         };
 
         /**
+         * @brief Defines a stand with all relevant information
+         */
+        struct Stand {
+            std::string                            name;                /**< The stand's name */
+            types::Coordinate                      position;            /**< The stand's coordinate */
+            types::Length                          assignmentRadius;    /**< The stand's automatic assignment radius */
+            int                                    priority;            /**< The generic stand priority during the assignment */
+            std::vector<std::string>               blockingStands;      /**< All stands that are blocked if this stand is occupied */
+            bool                                   manualAssignment;    /**< This stand is ignored by the automatic assignment */
+            types::Length                          wingspan[2];         /**< The allowed aircraft wingspan range */
+            types::Length                          length[2];           /**< The allowed aircraft length range */
+            types::Length                          height[2];           /**< The allowed aircraft height range */
+            std::list<types::Aircraft::WTC>        wtcWhitelist;        /**< All allowed WTC categories */
+            std::list<types::Aircraft::WTC>        wtcBlacklist;        /**< All non-allowed WTC categories */
+            std::list<types::Aircraft::EngineType> engineTypeWhitelist; /**< All allowed engine types */
+            std::list<types::Aircraft::EngineType> engineTypeBlacklist; /**< All non-allowed engine types */
+        };
+
+        /**
          * @brief Describes a airport configuration
          * @ingroup types
          */
@@ -47,6 +67,7 @@ namespace topskytower {
             bool                                               valid;                  /**< Marks if the configuration is valid */
             std::map<std::string, StandardInstrumentDeparture> sids;                   /**< The SIDs per airports */
             std::list<DestinationConstraint>                   destinationConstraints; /**< The constraints of specific destinations */
+            std::list<Stand>                                   aircraftStands;         /**< The list of all available aircraft stands */
 
             /**
              * @brief Creates an empty and unintialized airport configuration
@@ -54,7 +75,8 @@ namespace topskytower {
             AirportConfiguration() :
                     valid(false),
                     sids(),
-                    destinationConstraints() { }
+                    destinationConstraints(),
+                    aircraftStands() { }
         };
     }
 }
