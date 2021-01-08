388d5047a3dec853c7e45c3dd2563e237fa0cf05
Sven Czarnian <devel@svcz.de>
2021-01-08 22:08:28 +0100

    fix a typo

--- euroscope/PlugIn.cpp
+++ euroscope/PlugIn.cpp
@@ -340,7 +340,7 @@ void PlugIn::OnGetTagItem(EuroScopePlugIn::CFlightPlan flightPlan, EuroScopePlug
                 }
             }
             else {
-                std::strcpy(itemString, "UKN");
+                std::strcpy(itemString, "UNK");
                 *colorCode = EuroScopePlugIn::TAG_COLOR_EMERGENCY;
             }
         }
