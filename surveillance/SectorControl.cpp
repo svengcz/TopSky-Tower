e5d3f796297d73df4f10f8eda128eb9b9b66ea75
Sven Czarnian <devel@svcz.de>
2021-01-09 10:39:08 +0100

    delete old flight plan checks if the new one is completely invalid

--- surveillance/SectorControl.cpp
+++ surveillance/SectorControl.cpp
@@ -633,8 +633,8 @@ void SectorControl::updateFlight(const types::Flight& flight) {
     }
     /* check if we have to remove the handoff information */
     else if (this->m_handoffs.end() != it && true == it->second.handoffPerformed) {
-        /* find the current online controller */
-        auto currentNode = this->findOnlineResponsible(flight, flight.currentPosition(), false);
+        /* find the current online controller but ignore the clearance flag to avoid too early deletions, if Delivery is online */
+        auto currentNode = this->findOnlineResponsible(flight, flight.currentPosition(), ignoreClearanceFlag);
 
         /* check if an other controller is responsible */
         if (currentNode != this->m_ownSector && false == flight.isTracked())
