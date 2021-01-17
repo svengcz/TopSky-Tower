f281028009afa5f560835b0a31550d35b633f8f7
Sven Czarnian <devel@svcz.de>
2021-01-17 21:33:00 +0100

    introduce an initial implementation of the table viewer

---
+++ euroscope/ui/elements/TableViewer.cpp
@@ -0,0 +1,120 @@
+/*
+ * Author:
+ *   Sven Czarnian <devel@svcz.de>
+ * Brief:
+ *   Implements the table viewer
+ */
+
+#include "stdafx.h"
+
+#include "TableViewer.h"
+
+using namespace topskytower;
+using namespace topskytower::euroscope;
+
+TableViewer::TableViewer(RadarScreen* parent, const std::vector<std::string>& header, const Gdiplus::RectF& dimension) :
+        UiElement(parent, dimension),
+        m_header(),
+        m_rows() {
+    this->m_header.reserve(header.size());
+
+    for (const auto& row : std::as_const(header)) {
+        this->m_header.push_back(Text());
+        this->m_header.back().setText(row);
+        this->m_header.back().setFontColor(UiElement::backgroundColor());
+    }
+}
+
+std::size_t TableViewer::numberOfRows() const {
+    return this->m_rows.size();
+}
+
+std::size_t TableViewer::numberOfColumns() const {
+    return this->m_header.size();
+}
+
+void TableViewer::addRow() {
+    this->m_rows.push_back(std::move(std::vector<Text>(this->m_header.size())));
+}
+
+void TableViewer::removeRow(std::size_t idx) {
+    if (idx < this->m_rows.size()) {
+        auto it = this->m_rows.begin();
+        std::advance(it, idx);
+        this->m_rows.erase(it);
+    }
+}
+
+void TableViewer::setElement(std::size_t rowIdx, std::size_t columnIdx, const std::string& element) {
+    if (rowIdx < this->m_rows.size() && columnIdx < this->m_header.size()) {
+        auto it = this->m_rows.begin();
+        std::advance(it, rowIdx);
+        (*it)[columnIdx].setText(element);
+    }
+}
+
+bool TableViewer::visualize(Gdiplus::Graphics* graphics) {
+    /* contains the per-column widths */
+    std::vector<float> columnWidths(this->m_header.size(), 0.0f);
+    std::size_t emptyLines = 0;
+
+    /* get the widths of the header */
+    for (std::size_t i = 0; i < this->m_header.size(); ++i)
+        columnWidths[i] = std::max(columnWidths[i], this->m_header[i].rectangle().Width);
+
+    /* get the width of every row element */
+    for (const auto& row : std::as_const(this->m_rows)) {
+        bool emptyRow = true;
+
+        for (std::size_t i = 0; i < row.size(); ++i) {
+            columnWidths[i] = std::max(columnWidths[i], row[i].rectangle().Width);
+            if (false == helper::Math::almostEqual(0.0f, row[i].rectangle().Width))
+                emptyRow = false;
+        }
+
+        if (true == emptyRow)
+            emptyLines += 1;
+    }
+
+    /* get the overall width */
+    float overallWidth = 0.0f;
+    for (const auto& width : std::as_const(columnWidths))
+        overallWidth += width;
+
+    this->m_area = Gdiplus::RectF(this->m_area.X, this->m_area.Y, overallWidth,
+                                  (this->m_rows.size() - emptyLines + 1) * this->m_header[0].rectangle().Height);
+
+    float offsetX = this->m_area.X;
+    float offsetY = this->m_area.Y;
+
+    /* draw the background of the header */
+    Gdiplus::SolidBrush headerBrush(UiElement::foregroundColor());
+    graphics->FillRectangle(&headerBrush, Gdiplus::RectF(offsetX, offsetY, overallWidth, this->m_header[0].rectangle().Height));
+
+    /* draw the header */
+    for (std::size_t i = 0; i < this->m_header.size(); ++i) {
+        this->m_header[i].setPosition(Gdiplus::PointF(offsetX, offsetY));
+        this->m_header[i].setGraphics(graphics);
+        this->m_header[i].visualize();
+        offsetX += columnWidths[i];
+    }
+    offsetY += this->m_header[0].rectangle().Height;
+
+    /* draw the rows */
+    Gdiplus::Pen pen(UiElement::foregroundColor(), 1.0f);
+    for (auto& row : this->m_rows) {
+        offsetX = this->m_area.X;
+
+        for (std::size_t i = 0; i < this->m_header.size(); ++i) {
+            row[i].setPosition(Gdiplus::PointF(offsetX, offsetY));
+            row[i].setGraphics(graphics);
+            row[i].visualize();
+            offsetX += columnWidths[i];
+        }
+
+        offsetY += this->m_header[0].rectangle().Height;
+        graphics->DrawLine(&pen, Gdiplus::PointF(this->m_area.X, offsetY), Gdiplus::PointF(this->m_area.X + overallWidth, offsetY));
+    }
+
+    return true;
+}
---
+++ euroscope/ui/elements/TableViewer.h
@@ -0,0 +1,75 @@
+/*
+ * @brief Defines a viewer to visualize table information
+ * @file ui/elements/TableViewer.h
+ * @author Sven Czarnian <devel@svcz.de>
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "Text.h"
+#include "UiElement.h"
+
+namespace topskytower {
+    namespace euroscope {
+        class RadarScreen;
+
+        /**
+         * @brief Defines the table viewer
+         * @ingroup euroscope
+         */
+        class TableViewer : public UiElement {
+        private:
+            std::vector<Text>            m_header;
+            std::list<std::vector<Text>> m_rows;
+
+        public:
+            /**
+             * @brief Creates a new text viewer
+             * @param[in] parent The parent RADAR screen
+             * @param[in] header The header definition
+             * @param[in] dimension The viewer's dimension
+             */
+            TableViewer(RadarScreen* parent, const std::vector<std::string>& header, const Gdiplus::RectF& dimension);
+            /**
+             * @brief Destroys the button
+             */
+            virtual ~TableViewer() { }
+
+            /**
+             * @brief Returns the number of rows without the headline
+             * @return The number of rows
+             */
+            std::size_t numberOfRows() const;
+            /**
+             * @brief Returns the number of columns
+             * @return The column count
+             */
+            std::size_t numberOfColumns() const;
+            /**
+             * @brief Adds an empty row
+             */
+            void addRow();
+            /**
+             * @brief Removes a row
+             * @param[in] idx The row's index
+             */
+            void removeRow(std::size_t idx);
+            /**
+             * @brief Sets the element in the table
+             * @param[in] rowIdx The row index
+             * @param[in] columnIdx The column index
+             * @param[in] element The new content
+             */
+            void setElement(std::size_t rowIdx, std::size_t columnIdx, const std::string& element);
+            /**
+             * @brief Visualizes the toolbar with all active sub-menus
+             * @param[in] graphics The graphics container
+             * @return True if the element was visualized, else false
+             */
+            bool visualize(Gdiplus::Graphics* graphics) override;
+        };
+    }
+}
