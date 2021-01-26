ccb4d17e71d36b4d58049f57eaef260619b1bac8
Sven Czarnian <devel@svcz.de>
2021-01-26 12:35:18 +0100

    fix some bugs in the time conversion functions

--- include/helper/Time.h
+++ include/helper/Time.h
@@ -37,34 +37,41 @@ namespace topskytower {
             }
 
             /**
-             * @brief Converts a string into a std::chrono::system_clock::time_point
+             * @brief Converts a string into a std::chrono::system_clock::time_point in UTC
              * The function assumes the following format:
-             * - %Y-%m-%dT%H:%M:%S.000Z
+             * - %y%m%d%H%M
              * @param[in] date The string containing the timepoint
              * @return The converted time
              */
             static __inline std::chrono::system_clock::time_point stringToTime(const std::string& date) {
-                /* convert the time */
-                std::istringstream stream(date);
+                if (10 > date.length())
+                    return std::chrono::system_clock::time_point();
+
+                /* convert the string to a string with spaces due to an MSVC-runtime bug */
+                std::string convertedDate;
+                for (int i = 0; i < 10; i += 2)
+                    convertedDate += date.substr(i, 2) + " ";
+                std::istringstream stream(convertedDate);
+
                 struct std::tm tm;
-                stream >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S.000Z");
+                stream >> std::get_time(&tm, "%y %m %d %H %M");
                 return std::chrono::system_clock::from_time_t(std::mktime(&tm));
             }
 
             /**
              * @brief Converts a time point into a string
              * The function converts into the following format:
-             * - %Y-%m-%dT%H:%M:%S.000Z
-             * @param[in] time The timepoint which needs to be converted
+             * - %y%m%d%H%M
+             * @param[in] time The timepoint which needs to be converted in UTC
              * @return The converted time
              */
             static __inline std::string timeToString(const std::chrono::system_clock::time_point& time) {
-                char buf[256];
+                char buf[11];
 
                 std::time_t value = std::chrono::system_clock::to_time_t(time);
                 std::tm local = *std::localtime(&value);
 
-                std::strftime(buf, 256, "%Y-%m-%dT%H:%M:%S.000Z", &local);
+                std::strftime(buf, 11, "%y%m%d%H%M", &local);
 
                 return std::string(buf);
             }
